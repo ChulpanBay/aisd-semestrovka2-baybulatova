3e5b0837e700ef90bde72d115f2c4054eeb3d21e
ChulpanBay <chulpanb.12@mail.ru>
2026-05-12 23:15:15 +0300

    -

--- SplayTree/src/Main.java
+++ SplayTree/src/Main.java
@@ -10,26 +10,44 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
-    public static Tree<Integer> splayTree;
+    public static SplayTree<Integer> splayTree;
     public static List<String> list = new ArrayList<>();
     public static List<Long> listTimeOfInsert = new ArrayList<>();
     public static List<Long> listTimeOfDelete = new ArrayList<>();
     public static List<Long> listTimeOfSearch = new ArrayList<>();
+
+    // Lists for iteration counts
+    public static List<Integer> listIterationsOfInsert = new ArrayList<>();
+    public static List<Integer> listIterationsOfSearch = new ArrayList<>();
+    public static List<Integer> listIterationsOfDelete = new ArrayList<>();
+
     public static Integer AverageTimeInsert;
     public static Integer AverageTimeSearch;
     public static Integer AverageTimeDelete;
+    public static Integer AverageIterationsInsert;
+    public static Integer AverageIterationsSearch;
+    public static Integer AverageIterationsDelete;
+
     public static void main(String[] args) throws IOException {
-        //writeToFile();
         readFromFile();
         insertAllElements();
         searchHundredElements();
         deleteThousandElements();
+
         searchAverageTimeInsert();
-        System.out.println("Среднее значение добавления элемента " + AverageTimeInsert + " наносекунд");
+        searchAverageIterationsInsert();
         searchAverageTimeSearch();
-        System.out.println("Среднее значение поиска элемента " + AverageTimeSearch + " наносекунд");
+        searchAverageIterationsSearch();
         searchAverageTimeDelete();
-        System.out.println("Среднее значение удаления элемента " + AverageTimeDelete+ " наносекунд");
+        searchAverageIterationsDelete();
+
+        System.out.println("Среднее время добавления элемента: " + AverageTimeInsert + " наносекунд");
+        System.out.println("Среднее время поиска элемента: " + AverageTimeSearch + " наносекунд");
+        System.out.println("Среднее время удаления элемента: " + AverageTimeDelete + " наносекунд");
+
+        System.out.println("Среднее количество итераций при добавлении элемента: " + AverageIterationsInsert);
+        System.out.println("Среднее количество итераций при поиске элемента: " + AverageIterationsSearch);
+        System.out.println("Среднее количество итераций при удалении элемента: " + AverageIterationsDelete);
     }
 
     public static int[] createArray(){
@@ -39,6 +57,7 @@ public class Main {
         }
         return array;
     }
+
     public static void writeToFile() throws IOException {
         int[] dataArray = createArray();
         FileWriter writer = new FileWriter("example.txt",false);
@@ -48,19 +67,23 @@ public class Main {
         }
         writer.close();
     }
+
     public static void readFromFile() throws IOException {
         Path path = Path.of("example.txt");
         list = Files.readAllLines(path);
     }
+
     public static void insertAllElements(){
         splayTree = new SplayTree<>();
         for (String s : list) {
             long startTime = System.nanoTime();
             splayTree.insert(Integer.parseInt(s));
             long endTime = System.nanoTime();
-            listTimeOfInsert.add(endTime-startTime);
+            listTimeOfInsert.add(endTime - startTime);
+            listIterationsOfInsert.add(splayTree.getInsertIterations());
         }
     }
+
     public static void deleteThousandElements(){
         Random random = new Random();
         for (int i = 0; i < 1000; i++){
@@ -68,9 +91,11 @@ public class Main {
             long startTime = System.nanoTime();
             splayTree.delete(Integer.parseInt(list.get(randomIndex)));
             long endTime = System.nanoTime();
-            listTimeOfDelete.add(endTime-startTime);
+            listTimeOfDelete.add(endTime - startTime);
+            listIterationsOfDelete.add(splayTree.getDeleteIterations());
         }
     }
+
     public static void searchHundredElements(){
         Random random = new Random();
         for (int i = 0; i < 100; i++){
@@ -78,28 +103,56 @@ public class Main {
             long startTime = System.nanoTime();
             splayTree.search(Integer.parseInt(list.get(randomValue)));
             long endTime = System.nanoTime();
-            listTimeOfSearch.add(endTime-startTime);
+            listTimeOfSearch.add(endTime - startTime);
+            listIterationsOfSearch.add(splayTree.getSearchIterations());
         }
     }
+
     public static void searchAverageTimeInsert(){
-        int localSum = 0;
+        long localSum = 0;
         for (Long aLong : listTimeOfInsert) {
             localSum += aLong;
         }
-        AverageTimeInsert = localSum / listTimeOfInsert.size();
+        AverageTimeInsert = (int)(localSum / listTimeOfInsert.size());
     }
+
+    public static void searchAverageIterationsInsert(){
+        long localSum = 0;
+        for (Integer iterations : listIterationsOfInsert) {
+            localSum += iterations;
+        }
+        AverageIterationsInsert = (int)(localSum / listIterationsOfInsert.size());
+    }
+
     public static void searchAverageTimeSearch(){
-        int localSum = 0;
+        long localSum = 0;
         for (Long aLong : listTimeOfSearch) {
             localSum += aLong;
         }
-        AverageTimeSearch = localSum / listTimeOfSearch.size();
+        AverageTimeSearch = (int)(localSum / listTimeOfSearch.size());
     }
+
+    public static void searchAverageIterationsSearch(){
+        long localSum = 0;
+        for (Integer iterations : listIterationsOfSearch) {
+            localSum += iterations;
+        }
+        AverageIterationsSearch = (int)(localSum / listIterationsOfSearch.size());
+    }
+
     public static void searchAverageTimeDelete(){
-        int localSum = 0;
+        long localSum = 0;
         for (Long aLong : listTimeOfDelete) {
             localSum += aLong;
         }
-        AverageTimeDelete = localSum / listTimeOfDelete.size();
+        AverageTimeDelete = (int)(localSum / listTimeOfDelete.size());
+    }
+
+    public static void searchAverageIterationsDelete(){
+        long localSum = 0;
+        for (Integer iterations : listIterationsOfDelete) {
+            localSum += iterations;
+        }
+        AverageIterationsDelete = (int)(localSum / listIterationsOfDelete.size());
     }
 }
